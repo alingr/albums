caa9cdc35c78272b9af2402bfe65c43324dea1a9
alingr <groza.alin88@gmail.com>
2023-03-26 14:43:06 +0300

    Refactored DB connect function

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,17 +3,21 @@ import { MongoClient } from "mongodb";
 const username = process.env.db_user;
 const password = process.env.db_pass;
 
+// Create connection string
 const uri = "mongodb+srv://" + username + ":" + password + "@addressbook.f1grx.mongodb.net/?retryWrites=true&w=majority";
 
+// Create client
 const client = new MongoClient(uri);
 
-let conn;
+// Create DB connection
+let connection;
 try {
-  conn = await client.connect();
+  connection = await client.connect();
 } catch(e) {
   console.error(e);
 }
 
-let db = conn.db("music");
+// Connect to music connection
+let db = connection.db("music");
 
 export default db;
